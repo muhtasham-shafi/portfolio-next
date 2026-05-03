eb8a15e9c872c3d36033007a822c2b4e8d9ba844
Muhtasham.Shafi <141025831+muhtasham-shafi@users.noreply.github.com>
2026-05-03 23:04:07 +0600

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,10 @@
+import { MetadataRoute } from 'next';
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://muhtashamshafi.com',
+      lastModified: new Date(),
+    },
+  ];
+}
